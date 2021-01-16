1ba7633163de00ca63901acba074684c5534439c
campween <cmpbllowen@gmail.com>
2021-01-16 15:21:27 -0500

    safety commit

--- McGameJam 2021/Assets/Scripts/Boon.cs
+++ McGameJam 2021/Assets/Scripts/Boon.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class Boon
 {
-    public int count;           // ids place in boon array, 0-3. 
-                                // id corresponds to room in which the boon was acquired (0 comes at the end of the intro sequence)
     public string effect;       // which value does the boon affect?
     public double modifier;     // value by which the corresponding stat is changed
 
---
+++ McGameJam 2021/Assets/Scripts/BoonReader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoonReader : MonoBehaviour
+{
+    private Entity_Fundamental player;
+
+    void Awake()
+    {
+        player = GetComponent<Entity_Fundamental>();
+
+        for (int i = 0; i <= BoonManager.Instance.boonListPos; i++)
+        {
+            ApplyBoon(BoonManager.Instance.boonList[i]);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+}
