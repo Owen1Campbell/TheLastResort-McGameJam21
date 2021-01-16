572758800816699662f3824d504a16d44eca35b8
Jacob Dyer <dyerj@rpi.edu>
2021-01-15 21:15:40 -0500

    Entity_Fundamental + User_Controllable, Beginning Projectiles
    Separated concerns of stat and modifier tracking from movement from the user controller.
    
    Started work on projectiles, but there's far more to do with that.

---
+++ McGameJam 2021/Assets/Projectile.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Fundamental : MonoBehaviour
+{
+    // Fundamentals for Coding and Operation.
+    private Rigidbody2D projectileBody2D;
+
+    // Projectile Stats -- Managed and set by whatever creates the projectile.
+    public float movementSpeed;
+    public float baseDamage;
+    public float projectileDuration;
+    public Vector2 moveDirection;
+
+    // Projectile Mods
+    public float movementMultiplier;
+    public float damageMultiplier;
+
+    // Initialization.
+    void Start()
+    {
+        // Get Components
+        projectileBody2D = GetComponent<Rigidbody2D>();
+
+        // Invoke self-destruct after a particular time if time is set.
+        if (projectileDuration > 0)
+        {
+            Invoke("SelfDestruct", projectileDuration);
+        }
+    }
+
+    // Once-per-frame.
+    void Update()
+    {
+        // N/A.
+    }
+
+    private void FixedUpdate()
+    {
+        projectileBody2D.MovePosition(projectileBody2D.position + moveDirection * Time.fixedDeltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Inflict damage to the recipient if they can feel pain.
+        // TODO
+
+        // Self destruct after the projectile hits anything.
+        SelfDestruct();
+    }
+
+    // To be called after a particular amount of time, if needed.
+    private void SelfDestruct()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ McGameJam 2021/Assets/User_Controllable.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class User_Controllable : MonoBehaviour
+{
+    // Fundamentals for Operation.
+    private Vector2 moveDirection;
+
+    // Components this can work with:
+    private Entity_Fundamental entityBase;
+    private Rigidbody2D entityBody;
+
+    // Initialization.
+    void Start()
+    {
+        // Read in from other component(s), if any.
+        entityBase = GetComponent<Entity_Fundamental>();
+        entityBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Once-per-frame.
+    void Update()
+    {
+        // Take movement input and adjust the moveDirection vector.
+        // For where movement occurs, see 'FixedUpdate()'.
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        if (entityBase != null)
+        {
+            moveDirection = new Vector2(
+                (horizontalInput * entityBase.movementSpeed) * entityBase.movementMultiplier,
+                (verticalInput * entityBase.movementSpeed) * entityBase.movementMultiplier
+            );
+        } else {
+            moveDirection = new Vector2(horizontalInput, verticalInput);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(entityBody != null)
+        {
+            entityBody.MovePosition(entityBody.position + moveDirection * Time.fixedDeltaTime);
+        }
+    }
+}
