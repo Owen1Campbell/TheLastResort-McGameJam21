b593476b5016d37847dc9cae4987d047dce0b681
cpilip <pilipchristina@gmail.com>
2021-01-17 12:43:00 -0500

    Player death

---
+++ McGameJam 2021/Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    public GameObject endScreen;
+
+    public void OnDisable()
+    {
+        endScreen.SetActive(true);
+    }
+}
